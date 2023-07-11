e93d8add17c897a9a1c8c9ecb782687952b630a1
Anderson Ferraz <andmferraz12@gmail.com>
2023-07-11 19:23:57 -0300

    Task creation implementation

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,8 @@
 import { buildRoutePath } from './utils/build-route-path.js'
+import { randomUUID } from 'node:crypto'
+import { Database } from './database.js'
+
+const database = new Database()
 
 export const routes = [
     {
@@ -11,8 +15,21 @@ export const routes = [
     {
         method: 'POST',
         path: buildRoutePath('/tasks'),
-        handler: (req, res) => {            
-            return res.end('POST')
+        handler: (req, res) => {   
+            const { title, description } = req.body
+
+            const task = {
+                id: randomUUID(),
+                title,
+                description,
+                completed_at: null,
+                created_at: new Date().toLocaleDateString(),
+                updated_at: new Date().toLocaleDateString()
+            }
+
+            database.insert('tasks', task)
+
+            return res.writeHead(201).end()
         }
     },
     {
